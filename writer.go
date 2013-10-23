890fd86fc28e4d5bbecaa63d36adcfdee7ca9b3b
Wilson Lee <kourge@gmail.com>
2013-10-23 01:55:45 -0700

    Remove redundant Write method

--- writer.go
+++ writer.go
@@ -18,11 +18,6 @@ func (w *Writer) WriteEntry(entry *Entry) (written int, err error) {
 	return fmt.Fprintf(w, "%s\n", entry.String())
 }
 
-func (e *Entry) Write(dst io.Writer) (written int, err error) {
-	w := &Writer{dst}
-	return w.WriteEntry(e)
-}
-
 func Dump(filename string, data map[string]string) (written int, err error) {
 	file, err := os.Create(filename)
 	if err != nil {
