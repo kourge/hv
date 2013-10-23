818fb6c8fd41a007ecc5d5622ab4d3561f0c7948
Wilson Lee <kourge@gmail.com>
2013-10-23 01:54:10 -0700

    Discard toy Iter implementation and use callbacks instead

--- reader.go
+++ reader.go
@@ -35,20 +35,17 @@ func (r *Reader) ReadEntry() (entry *Entry, err error) {
 	return
 }
 
-func (r *Reader) Iter() chan *Entry {
-	ch := make(chan *Entry)
-	go func() {
-		for {
-			if entry, err := r.ReadEntry(); err != nil {
-				break
-			} else if entry != nil {
-				ch <- entry
-			}
+func (r *Reader) Each(f func(*Entry)) (err error) {
+	for {
+		if e, err := r.ReadEntry(); err == io.EOF {
+			break
+		} else if err != nil {
+			return err
+		} else if e != nil {
+			f(e)
 		}
-		close(ch)
-	}()
-
-	return ch
+	}
+	return
 }
 
 func Load(filename string) (m map[string]string, err error) {
@@ -57,18 +54,12 @@ func Load(filename string) (m map[string]string, err error) {
 		return
 	}
 
-	r := NewReader(file)
 	m = make(map[string]string)
-	for {
-		if e, err := r.ReadEntry(); err == io.EOF {
-			break
-		} else if err != nil {
-			return m, err
-		} else if e != nil {
-			m[e.Filename] = e.Checksum
-		}
-	}
+	r := NewReader(file)
+	err = r.Each(func(e *Entry) {
+		m[e.Filename] = e.Checksum
+	})
 
-	return m, nil
+	return m, err
 }
 
