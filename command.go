ed9d3250c6b8c602012a6488be1b5aa1494cde75
Wilson Lee <kourge@gmail.com>
2013-10-26 02:52:20 -0700

    Add a Command struct type for subcommand management

---
+++ command.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"os"
+	"io"
+	"fmt"
+	"flag"
+	"strings"
+)
+
+type Command struct {
+	Run func(cmd *Command, args []string)
+	Flag flag.FlagSet
+
+	Usage string
+	Short string
+	Long string
+}
+
+func (c *Command) Name() (name string) {
+	name = c.Usage
+	i := strings.Index(name, " ")
+	if i >= 0 {
+		name = name[:i]
+	}
+	return
+}
+
+func (c *Command) Runnable() bool {
+	return c.Run != nil
+}
+
+func (c *Command) PrintUsage(w io.Writer) {
+	program := os.Args[0]
+
+	if w == nil {
+		w = os.Stderr
+	}
+	if c.Runnable() {
+		fmt.Fprintf(w, "Usage: %s %s\n", program, c.Usage)
+		fmt.Fprintf(w, "%s\n\n", c.Short)
+		c.Flag.PrintDefaults()
+	}
+	fmt.Fprintln(w, strings.TrimRight(c.Long, "\n"))
+}
+
