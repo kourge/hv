a99a827d1a5224afac313be2391dc53320680702
Wilson Lee <kourge@gmail.com>
2013-12-08 19:28:24 -0800

    Move Entries utility code to entries.go

---
+++ entries.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"os"
+	"io/ioutil"
+	"container/list"
+)
+
+type Entries []*Entry
+
+func EntriesFromPath(path string) (entries Entries, err error) {
+	files, err := ioutil.ReadDir(cwd)
+	if err != nil {
+		return
+	}
+	return EntriesFromFiles(files), nil
+}
+
+func EntriesFromFiles(files []os.FileInfo) Entries {
+	items := 0
+	entries := make([]*Entry, len(files))
+	for _, file := range files {
+		if matches(file) {
+			entry := &Entry{Filename: file.Name()}
+			entries[items] = entry
+			items++
+		}
+	}
+
+	return entries[0:items]
+}
+
+func EntriesFromChecksumFile(checksums *os.File) (entries Entries, err error) {
+	entries = make([]*Entry, 0)
+	r := NewReader(checksums)
+	err = r.Each(func(entry *Entry) {
+		entries = append(entries, entry)
+	})
+	return
+}
+
+type BucketsByChecksum map[string]*list.List
+
+func (entries Entries) BucketsByChecksum() (buckets BucketsByChecksum) {
+	buckets = make(map[string]*list.List)
+	for _, entry := range entries {
+		bucket, exists := buckets[entry.Checksum]
+		if !exists {
+			bucket = list.New()
+			buckets[entry.Checksum] = bucket
+		}
+		bucket.PushBack(entry.Filename)
+	}
+	return
+}
+
--- entry.go
+++ entry.go
@@ -3,11 +3,9 @@ package main
 import (
 	"os"
 	"io"
-	"io/ioutil"
 	"fmt"
 	"strings"
 	"crypto"
-	"container/list"
 )
 
 type Entry struct {
@@ -62,53 +60,5 @@ func matches(file os.FileInfo) bool {
 	startsWith, endsWith := strings.HasPrefix, strings.HasSuffix
 	name, mode := file.Name(), file.Mode()
 	return mode.IsRegular() && !startsWith(name, ".") && !endsWith(name, "SUMS")
-}
-
-type Entries []*Entry
-
-func EntriesFromPath(path string) (entries Entries, err error) {
-	files, err := ioutil.ReadDir(cwd)
-	if err != nil {
-		return
-	}
-	return EntriesFromFiles(files), nil
-}
-
-func EntriesFromFiles(files []os.FileInfo) Entries {
-	items := 0
-	entries := make([]*Entry, len(files))
-	for _, file := range files {
-		if matches(file) {
-			entry := &Entry{Filename: file.Name()}
-			entries[items] = entry
-			items++
-		}
-	}
-
-	return entries[0:items]
-}
-
-func EntriesFromChecksumFile(checksums *os.File) (entries Entries, err error) {
-	entries = make([]*Entry, 0)
-	r := NewReader(checksums)
-	err = r.Each(func(entry *Entry) {
-		entries = append(entries, entry)
-	})
-	return
-}
-
-type BucketsByChecksum map[string]*list.List
-
-func (entries Entries) BucketsByChecksum() (buckets BucketsByChecksum) {
-	buckets = make(map[string]*list.List)
-	for _, entry := range entries {
-		bucket, exists := buckets[entry.Checksum]
-		if !exists {
-			bucket = list.New()
-			buckets[entry.Checksum] = bucket
-		}
-		bucket.PushBack(entry.Filename)
-	}
-	return
 }
 
